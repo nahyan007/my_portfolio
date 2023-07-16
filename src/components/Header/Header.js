a6be222d973d6d88cab67ed1a3e4a816b6f2a479
nahyan007 <shorifrex@gmail.com>
2023-07-16 21:33:03 +0600

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,70 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+
+    <Div3>
+      <SocialIcons target="_blank" href="https://github.com/nahyan007">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons
+        target="_blank"
+        href="https://www.linkedin.com/in/al-ashraful-islam-shorif-8213b5267"
+      >
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons
+        target="_blank"
+        href="mailto:alashrafulislamshorif@gmail.com"
+      >
+        <AiFillMail size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
